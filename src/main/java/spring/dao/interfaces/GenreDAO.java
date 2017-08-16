01cfa6ebe011b1104dc9ee616bd59e3757755f90
Yahor Melnik <Egorpyp@gmail.com>
2017-08-16 14:47:26 +0300

    basic UI

---
+++ src/main/java/spring/dao/interfaces/GenreDAO.java
@@ -0,0 +1,12 @@
+package spring.dao.interfaces;
+
+
+import spring.entities.Genre;
+
+import java.util.List;
+
+public interface GenreDAO {
+
+    List<Genre> getGenres();
+
+}
---
+++ src/main/webapp/resources/js/library.js
@@ -0,0 +1,4 @@
+function showProgress(){
+    btnUpdateBook.disabled=true;
+}
+
